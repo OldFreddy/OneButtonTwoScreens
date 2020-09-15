c9847e173b6390fc6f95a68e033b6a17df83ad4f
Freddy <oldfredddy@gmail.com>
2020-09-15 12:05:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/onebuttontwoscreens/textClass.java
@@ -0,0 +1,14 @@
+package com.example.onebuttontwoscreens;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class textClass extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_activity);
+    }
+}
